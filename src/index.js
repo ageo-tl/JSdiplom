279bcb3ae28b6d0e71fdedc68b790e27cea265ee
Anton Ivanov <geo.alpha.contact@gmail.com>
2019-12-02 23:11:14 +0300

    Отображение/скрытие модального окна .popup-discount (Заказ со скидкой)

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnsCallMeBack = document.querySelectorAll(".call-btn[href='#']");
 
 
+  // Discount price and order
+  const popupDiscount = document.querySelector(".popup-discount");
+  const btnsDiscount = document.querySelectorAll(".discount-btn");
+  const btnPriceOrder = document.querySelector(".construct-btn.call-btn");
+
+
 
   // ADD EVENT LISTENER
   document.addEventListener("click", (event) => {
@@ -28,6 +34,17 @@ document.addEventListener("DOMContentLoaded", () => {
       hidePopup(popupCall);
     }
 
+    // Discount price and order
+    if ([...btnsDiscount, btnPriceOrder].includes(target)) {
+      // popupDiscount.style.display = "block";
+      showPopup(popupDiscount);
+    }
+    if (target === popupDiscount ||
+      target === popupDiscount.querySelector(".popup-close")) {
+    event.preventDefault();
+    // popupDiscount.style.display = "none";
+    hidePopup(popupDiscount);
+  }
   });
 
 });
